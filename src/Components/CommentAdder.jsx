6d68d901288aa2f8b8ad278e3265b8e32bcc20df
Mariha-B <mariha_butt@hotmail.com>
2024-03-13 09:30:38 +0000

    Task 8, added loading for post comment

--- src/Components/CommentAdder.jsx
+++ src/Components/CommentAdder.jsx
@@ -3,7 +3,10 @@ import { postComment } from "../Utils/api";
 
 const CommentAdder =({article_id, setComments}) => {
 const [newComment, setNewComment] = useState('')
+const [isLoading, setIsLoading]=useState(false)
+
 const handleSubmit=(event)=>{
+    setIsLoading(true)
     event.preventDefault();
     const postBody = {
         author: "grumpy19",
@@ -14,21 +17,33 @@ const handleSubmit=(event)=>{
         setComments((currComments)=>{
             return [commentFromApi,...currComments]
         })
+        setIsLoading(false)
     }).catch((err)=>{
         console.log(err);
     })
 }
-
+if(isLoading){
+    return (
+        <form id='form'>
+        <h3>Add a comment...</h3>
+        <div className="input-container">
+            <p id='loading'>Posting your comment...</p>
+        </div>
+        <button type="submit">Submit Comment</button>
+    </form>
+        )
+}
+else{
 return (
     <form id='form' onSubmit={handleSubmit}>
         <h3>Add a comment...</h3>
         <div className="input-container">
-            <input type='text' placeholder="What's on your mind..." value={newComment} onChange={(event) => setNewComment(event.target.value)}></input>
+            <input minLength='5'type='text' required placeholder="What's on your mind..." value={newComment} onChange={(event) => setNewComment(event.target.value)}></input>
         </div>
         <button type="submit">Submit Comment</button>
     </form>
 )
 }
 
-
+}
 export default CommentAdder
